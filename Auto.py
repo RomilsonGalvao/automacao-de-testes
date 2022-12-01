8431b288e82dbea91dc4a9e0a58a956194df21e4
Romilson Galvão Mota Cardozo <romilson.gmc@gmail.com>
2022-12-01 16:10:46 -0300

    commit

--- Auto.py
+++ Auto.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 driver = webdriver.Firefox()
 driver.get('http://172.16.100.57:8080/Nexgen23/login.html')
-nexgen = driver.switch_to.window
-us = nexgen.find_element(By.XPATH, '//*[@id="txtUsuario"]')
-us.send_keys('791')
+driver.switch_to.window
+driver.find_element(By.XPATH, '//*[@id="txtUsuario"]')
+driver.send_keys('791')
 
