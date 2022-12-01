70bcdafeb609663616806f778371d3580e81ce16
Romilson Galvão Mota Cardozo <romilson.gmc@gmail.com>
2022-12-01 14:38:07 -0300

    comit

---
+++ Auto.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+driver = webdriver.Firefox()
+driver.get('http://172.16.100.57:8080/Nexgen23/')
--- aula03.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-noticia = requests.get('https://g1.globo.com')
-content = noticia.content
-site = BeautifulSoup(content, 'html.parser')
-postagem = site.find('div', attrs={'class': 'feed-post-body-title gui-color-primary gui-color-hover'})
-print(postagem.text)
