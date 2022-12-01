1cbc7c89d891b147f3ecb07a2cffdfb1971217ac
Romilson Galvão Mota Cardozo <romilson.gmc@gmail.com>
2022-12-01 16:04:18 -0300

    commit

--- Auto.py
+++ Auto.py
@@ -3,10 +3,8 @@ from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 driver = webdriver.Firefox()
-driver.get('http://172.16.100.57:8080/Nexgen23/')
-nexgen = driver.current_window_handle
-driver.find_element(By.ID, 'txtUsuario')
-sleep(5)
-for handle in driver.window_handles: 
-    if handle != nexgen: 
-        login_page = handle
+driver.get('http://172.16.100.57:8080/Nexgen23/login.html')
+nexgen = driver.switch_to.window
+us = nexgen.find_element(By.CSS_SELECTOR, '#txtUsuario')
+us.send_keys('791')
+
