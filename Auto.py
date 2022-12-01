96d9c0438418b7ecdbbf0f71d4ca200ed329691e
Romilson Galvão Mota Cardozo <romilson.gmc@gmail.com>
2022-12-01 16:04:48 -0300

    Update Auto.py

--- Auto.py
+++ Auto.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
-from time import sleep
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 driver = webdriver.Firefox()
 driver.get('http://172.16.100.57:8080/Nexgen23/login.html')
 nexgen = driver.switch_to.window
-us = nexgen.find_element(By.CSS_SELECTOR, '#txtUsuario')
+us = nexgen.find_element(By.XPATH, '//*[@id="txtUsuario"]')
 us.send_keys('791')
 
