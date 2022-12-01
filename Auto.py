4062ca354ec2c88a9483485414ca6c34bc264682
Romilson Galvão Mota Cardozo <romilson.gmc@gmail.com>
2022-12-01 15:11:25 -0300

    comit

--- Auto.py
+++ Auto.py
@@ -1,3 +1,12 @@
 from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 driver = webdriver.Firefox()
 driver.get('http://172.16.100.57:8080/Nexgen23/')
+nexgen = driver.current_window_handle
+driver.find_element(By.ID, 'txtUsuario')
+sleep(5)
+for handle in driver.window_handles: 
+    if handle != nexgen: 
+        login_page = handle
