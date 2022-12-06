a9f12b53ebb919127d637a3ccec1b1f92cab1b26
Romilson Galvão Mota Cardozo <romilson.gmc@gmail.com>
2022-12-06 19:26:03 -0300

    commit

--- Auto.py
+++ Auto.py
@@ -1,9 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 driver = webdriver.Firefox()
-driver.get('http://172.16.100.57:8080/Nexgen23/login.html')
-driver.switch_to.window
-driver.find_element(By.XPATH, '//*[@id="txtUsuario"]')
-driver.send_keys('791')
+driver.get_window_size('http://172.16.100.57:8080/Nexgen23/')
+
+
+time.sleep(5)
+WebDriverWait(driver, 15)
+usu = driver.find_element(By.XPATH, '//*[@id="txtUsuario"]')
+login = usu.send_keys('791')
+
+
+
+
+
+
 
